09c1476a28b8f79ba9d8e40ddeefe8d34341a937
Aditya Caprihan <acaprihan@atlassian.com>
2024-06-30 18:15:58 +1000

    Use Org 9.7.4

--- repos/elpa/elpa-generated.nix
+++ repos/elpa/elpa-generated.nix
@@ -4107,10 +4107,10 @@
       elpaBuild {
         pname = "org";
         ename = "org";
-        version = "9.7.6";
+        version = "9.7.4";
         src = fetchurl {
-          url = "https://elpa.gnu.org/packages/org-9.7.6.tar";
-          sha256 = "0pxjc2bydnzd31wg71nfh7zzf3mhsnzm2nd7p736bj1w0pvg89ng";
+          url = "https://elpa.gnu.org/packages/org-9.7.4.tar";
+          sha256 = "0rifr6v0b1skkh7a2ysapxv95azxrq0wqymbdp086v9p1h619awn";
         };
         packageRequires = [ emacs ];
         meta = {
